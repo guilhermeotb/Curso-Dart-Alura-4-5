a027fbe6ac32c16ad475c1760de63a5d90c5c2c0
Pereira <pereira@eb.mil.br>
2026-03-02 13:50:07 -0300

    #1 future response implementado

--- dart_assincronismo/bin/main.dart
+++ dart_assincronismo/bin/main.dart
@@ -5,5 +5,5 @@ void main(){
 
 resquestData(){
   String url = "https://gist.githubusercontent.com/guilhermeotb/b5e5988083befbf76bfc7e34c6f518d1/raw/8eb9c4615a551aa5f6eb119cec1a93c781759f5e/accounts.json";
- print( get(Uri.parse(url)));
+ Future<Response> futureResponse = get(Uri.parse(url));
 }
